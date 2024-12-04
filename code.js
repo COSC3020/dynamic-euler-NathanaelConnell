e985eaa65e72f70de8676f654429a4306371a9c3
Nathanael Connell <nathanael@Nathanaels-MacBook-Pro.local>
2024-12-03 17:30:17 -0700

    Updated Commit

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
-function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
-}
-
 function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    let factorial = 1; 
+    let sum = 1; 
+    for (let i = 1; i <= n; i++) {
+        factorial *= i; 
+        sum += 1.0 / factorial;
+    }
+    return sum;
 }
